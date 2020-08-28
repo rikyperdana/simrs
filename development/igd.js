91435069ddea8e7e0702721274c46082da1a401a
Riky Perdana <perdanariky@gmail.com>
2020-08-28 22:39:13 +0700

    crit fix inpatients

--- development/igd.js
+++ development/igd.js
@@ -123,6 +123,7 @@ _.assign(comp, {
     m('.box', m(autoForm({
       id: 'igdVisit', autoReset: true,
       schema: _.omit(schemas.rawatJalan, 'klinik'),
+      confirmMessage: 'Yakin untuk mendaftarkan pasien ke IGD?',
       action: doc => [
         updateBoth('patients', state.onePatient._id, _.assign(
           state.onePatient, {emergency:
